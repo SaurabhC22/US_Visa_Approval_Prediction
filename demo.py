b9a645ecae70ece0b4a225050cdcf599e0580650
Saurabh Chaudhari <109277240+SaurabhChaudhari22@users.noreply.github.com>
2024-06-09 11:07:23 +0530

    EDA and Feature Engg

--- demo.py
+++ demo.py
@@ -1,4 +1,17 @@
-from US_Visa.logger import logging
+# CODE FOR TEST LOGGING
+#from US_Visa.logger import logging
+#logging.info("Testing logging file")
 
-logging.info("Testing logging file")
+# CODE FOR TESTING EXCEPTION
+
+from US_Visa.logger import logging
+from US_Visa.exception import USvisaException
+import sys
+
+try:
+    a = 1 / "10"
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e,sys) from e
+
 
